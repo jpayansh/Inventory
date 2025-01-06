c9afdea9f041920086c90dba504972e130c00f17
jpayansh <payanshj9@gmail.com>
2025-01-06 14:51:11 +0530

    backend apiss

---
+++ src/app/api/orders/route.ts
@@ -0,0 +1,68 @@
+import { orders } from 'dbConfig/dbConfig';
+import { getUserTokenDetails } from 'helper/getUserToken';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest) {
+  try {
+    const userId = await getUserTokenDetails(request);
+
+    if (!userId) {
+      return NextResponse.json({ message: 'Invalid Token' }, { status: 400 });
+    }
+
+    return NextResponse.json(
+      { message: 'Success', data: orders },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+export async function POST(request: NextRequest) {
+  try {
+    const userId = await getUserTokenDetails(request);
+
+    if (!userId) {
+      return NextResponse.json({ message: 'Invalid Token' }, { status: 400 });
+    }
+
+    return NextResponse.json(
+      { message: 'Success', data: orders },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+export async function DELETE(request: NextRequest) {
+  try {
+    const userId = await getUserTokenDetails(request);
+
+    if (!userId) {
+      return NextResponse.json({ message: 'Invalid Token' }, { status: 400 });
+    }
+
+    return NextResponse.json(
+      { message: 'Success', data: orders },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+export async function PUT(request: NextRequest) {
+  try {
+    const userId = await getUserTokenDetails(request);
+
+    if (!userId) {
+      return NextResponse.json({ message: 'Invalid Token' }, { status: 400 });
+    }
+
+    return NextResponse.json(
+      { message: 'Success', data: orders },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
