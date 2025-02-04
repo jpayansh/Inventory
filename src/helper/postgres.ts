55fb2f37b98e34ce17bba81c281fc9fd25c63ee7
jpayansh <110517005+jpayansh@users.noreply.github.com>
2025-02-04 17:09:07 +0530

    Update postgres.ts

--- src/helper/postgres.ts
+++ src/helper/postgres.ts
@@ -1,11 +1,10 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  host: '127.0.0.1',
-  port: 5432,
-  user: 'node1',
-  password: '123',
-  database: 'inventorydb',
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
